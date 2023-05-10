3be18d04a47e07e85326f8de30c3916dd712915e
florius88 <florsantosbueno@gmail.com>
2023-05-10 19:30:33 +0200

    Menu de la app

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
+interface MenuItem {
+  name: string;
+  route: string;
+}
+
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
+
+  public menuItems: MenuItem[] = [
+    {name: 'Full-Screen', route: '/maps/fullscreen' },
+    {name: 'Zoom-Range', route: '/maps/zoom-range' },
+    {name: 'Markers', route: '/maps/markers' },
+    {name: 'Houses', route: '/maps/properties' },
+  ]
 
 }
