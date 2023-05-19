720dff3c94837583b793d2daf748ebb2394ec5e5
florius88 <florsantosbueno@gmail.com>
2023-05-19 13:12:37 +0200

    Standalone como pagina

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  // selector: 'app-alone-page',
+  // imports: [CommonModule],
+  standalone: true,
+  templateUrl: './alone-page.component.html',
+  styleUrls: ['./alone-page.component.css']
+})
+export class AlonePageComponent {
+
+}
